2d3505b497dcf174bc61aa6649abf094765e8f7c
uhstudio <uh20019@gmail.com>
2026-05-25 20:16:51 +0700

    Fix env

--- application/logs/log-2026-05-25.php
+++ application/logs/log-2026-05-25.php
@@ -4,3 +4,5 @@ ERROR - 2026-05-25 20:11:27 --> Severity: Warning --> mysqli::real_connect(): (H
 ERROR - 2026-05-25 20:11:27 --> Unable to connect to the database
 ERROR - 2026-05-25 20:12:01 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user ''@'localhost' (using password: NO) C:\xampp\htdocs\meeting-app\system\database\drivers\mysqli\mysqli_driver.php 211
 ERROR - 2026-05-25 20:12:01 --> Unable to connect to the database
+ERROR - 2026-05-25 20:15:34 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user ''@'localhost' (using password: NO) C:\xampp\htdocs\meeting-app\system\database\drivers\mysqli\mysqli_driver.php 211
+ERROR - 2026-05-25 20:15:34 --> Unable to connect to the database
